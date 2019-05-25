a092dc59d229c42a2b7eb1ed74287ea34f8f8a62
Mark Kibbe <mark.e.kibbe@gmail.com>
2019-05-25 16:26:52 -0400

    Added first script, button on form. Learning Team Explorer

--- PowerShellHelper/PowerShellHelper/Form1.Designer.cs
+++ PowerShellHelper/PowerShellHelper/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRunScript = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRunScript
+            // 
+            this.btnRunScript.Location = new System.Drawing.Point(193, 144);
+            this.btnRunScript.Name = "btnRunScript";
+            this.btnRunScript.Size = new System.Drawing.Size(382, 164);
+            this.btnRunScript.TabIndex = 0;
+            this.btnRunScript.Text = "Run That Script";
+            this.btnRunScript.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRunScript);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRunScript;
     }
 }
 
