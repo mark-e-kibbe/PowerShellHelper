fa5bf94b5f6a8044cd7e0e1f90b1b264a157bba0
Mark Kibbe <mark.e.kibbe@gmail.com>
2019-05-27 14:54:18 -0400

    Refactoring ongoing. Finished basic script runner class. Inserted Script runner object instantiation.
    Proceeding to test

--- PowerShellHelper/PowerShellHelper/Form1.cs
+++ PowerShellHelper/PowerShellHelper/Form1.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Management.Automation;
 using System.Collections.ObjectModel;
+using PowerShellHelper.Models;
 
 namespace PowerShellHelper
 {
@@ -22,22 +23,24 @@ namespace PowerShellHelper
 
         private void BtnRunScript_Click(object sender, EventArgs e)
         {
+            PowerShellScriptRunner scriptRunner = new PowerShellScriptRunner("TestScript.ps1");
+            scriptRunner.RunScript();
             //Get Powershell running with new assembly nuget pack
-            using (PowerShell shell = PowerShell.Create())
-            {
-                string fileName = "TestScript.ps1";
-                string path = Path.Combine(Environment.CurrentDirectory, @"Scripts\", fileName);
-                
-                //shell.AddCommand("Get-Content " + path + " | Invoke-Expression");
-                shell.AddCommand("Get-Content").AddArgument(path);
-                shell.AddCommand("Invoke-Expression");
-                shell.Invoke();
-                
-                Console.WriteLine("TEST");
-                // use "AddParameter" to add a single parameter to the last command/script on the pipeline.
-                //shell.AddParameter("param1", "parameter 1 value!");
+            //using (PowerShell shell = PowerShell.Create())
+            //{
+            //    string fileName = "TestScript.ps1";
+            //    string path = Path.Combine(Environment.CurrentDirectory, @"Scripts\", fileName);
 
-            }
+            //    //shell.AddCommand("Get-Content " + path + " | Invoke-Expression");
+            //    shell.AddCommand("Get-Content").AddArgument(path);
+            //    shell.AddCommand("Invoke-Expression");
+            //    shell.Invoke();
+
+            //    Console.WriteLine("TEST");
+            //    // use "AddParameter" to add a single parameter to the last command/script on the pipeline.
+            //    //shell.AddParameter("param1", "parameter 1 value!");
+
+            //}
 
             //Execute a powershell script
             //string finalPath = "Get-Content " + path + " | Invoke-Expression";
@@ -45,3 +48,4 @@ namespace PowerShellHelper
         }
     }
 }
+
--- PowerShellHelper/PowerShellHelper/Models/PowerShellScriptRunner.cs
+++ PowerShellHelper/PowerShellHelper/Models/PowerShellScriptRunner.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Management.Automation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +16,23 @@ namespace PowerShellHelper.Models
             fileName = scriptName;
         }
         
-        private string 
+        private string getCompletePath(string scriptName)
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, @"Scripts\", scriptName);
+
+            return path;
+        }
+
+        public void RunScript()
+        {
+            //Get Powershell running with new assembly nuget pack
+            using (PowerShell shell = PowerShell.Create())
+            {
+                string scriptPathToRun = getCompletePath(fileName);
+                shell.AddCommand("Get-Content").AddArgument(scriptPathToRun);
+                shell.AddCommand("Invoke-Expression");
+                shell.Invoke();
+            }
+        }
     }
 }
