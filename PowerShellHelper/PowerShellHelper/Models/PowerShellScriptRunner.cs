37e50c308ee8bb9b96d548b20045803d231eecca
Mark Kibbe <mark.e.kibbe@gmail.com>
2019-05-28 07:05:00 -0400

    Added XML commenting to Powershell class

--- PowerShellHelper/PowerShellHelper/Models/PowerShellScriptRunner.cs
+++ PowerShellHelper/PowerShellHelper/Models/PowerShellScriptRunner.cs
@@ -8,14 +8,31 @@ using System.Threading.Tasks;
 
 namespace PowerShellHelper.Models
 {
+    /// <summary>
+    /// Class for running powershell scripts
+    /// </summary>
     internal class PowerShellScriptRunner
     {
+        /// <summary>
+        /// fileName pertaining to the scripts filename and extension
+        /// <exampl>TestScript.ps1</exampl>
+        /// </summary>
         private string fileName = "";
+
+        /// <summary>
+        /// Constructor that creates the powershell script
+        /// </summary>
+        /// <param name="scriptName">Script file name with file extension</param>
         public PowerShellScriptRunner(string scriptName)
         {
             fileName = scriptName;
         }
         
+        /// <summary>
+        /// Gets and return the complete file path from the projects Scripts folder
+        /// </summary>
+        /// <param name="scriptName">Script filename with file extension</param>
+        /// <returns></returns>
         private string getCompletePath(string scriptName)
         {
             string path = Path.Combine(Environment.CurrentDirectory, @"Scripts\", scriptName);
@@ -23,6 +40,9 @@ namespace PowerShellHelper.Models
             return path;
         }
 
+        /// <summary>
+        /// Executes the powershell script that needs to be ran
+        /// </summary>
         public void RunScript()
         {
             //Get Powershell running with new assembly nuget pack
